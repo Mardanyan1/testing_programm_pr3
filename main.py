680e002af85b14dd06476fdbd8dccca40c3a3c58
Artyom <arkhamb@yandex.ru>
2021-11-05 14:09:29 +0300

    Добавление возможности изменения оценок

--- main.py
+++ main.py
@@ -2,6 +2,20 @@ class Students:
     subject = ["Math", "Physics", "Programming"]
     mark = [4, 5, 3]
 
+    def ch_mark(self, Mark):
+        self.mark = Mark
+
+
+def change_mark():
+    new_mark = []
+    print("введите количество оценок")
+    n_mark = int(input())
+    print("введите оценки")
+    while n_mark > 0:
+        n_mark = n_mark - 1
+        new_mark.append(input())
+    Egor.ch_mark(new_mark)
+
 
 Egor = Students()
 while 1:
@@ -10,3 +24,5 @@ while 1:
         print(Egor.subject)
     elif choice == 2:
         print(Egor.mark)
+    elif choice == 3:
+        change_mark()
